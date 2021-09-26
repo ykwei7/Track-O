576795b3c7ca86f63c35bdb524532ea2e24e283e
KengXIII <kengi0813@gmail.com>
2021-09-26 18:10:29 +0800

    Remove extra line in Class Level

--- src/main/java/seedu/address/model/person/Level.java
+++ src/main/java/seedu/address/model/person/Level.java
@@ -3,7 +3,6 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 
 import static seedu.address.commons.util.AppUtil.checkArgument;
-
 /**
  * Represents a Person's education level in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidLevel(String)}
