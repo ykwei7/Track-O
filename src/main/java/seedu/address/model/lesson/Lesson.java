e24b510103355282af33671c20a0a9996506d977
botr99 <brandonong999@gmail.com>
2021-10-07 12:26:07 +0800

    Set up Lesson

---
+++ src/main/java/seedu/address/model/lesson/Lesson.java
@@ -0,0 +1,64 @@
+package seedu.address.model.lesson;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.util.Objects;
+
+/**
+ * Represents a Lesson in the application.
+ */
+public class Lesson {
+
+    private final Subject subject;
+    private final Time time;
+
+    /**
+     * Constructs a {@code Lesson}.
+     *
+     * @param subject The subject of the lesson.
+     * @param time The time of the lesson.
+     */
+    public Lesson(Subject subject, Time time) {
+        requireAllNonNull(subject, time);
+        this.subject = subject;
+        this.time = time;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    /**
+     * Returns true if both lessons have the same subject and time.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Lesson)) {
+            return false;
+        }
+
+        Lesson otherLesson = (Lesson) other;
+        return otherLesson.getSubject().equals(getSubject())
+                && otherLesson.getTime().equals(getTime());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(subject, time);
+    }
+
+    @Override
+    public String toString() {
+        return subject + " " + time;
+    }
+
+}
---
+++ src/main/java/seedu/address/model/lesson/Time.java
@@ -0,0 +1,99 @@
+package seedu.address.model.lesson;
+
+import static seedu.address.commons.util.AppUtil.checkArgument;
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Objects;
+
+/**
+ * Represents the Time of a particular Lesson.
+ */
+public class Time {
+
+    public static final long MINIMUM_DURATION = 10;
+
+    public static final String MESSAGE_CONSTRAINTS_IMPROPER_TIME =
+            "Start time should be before end time";
+
+    public static final String MESSAGE_CONSTRAINTS_INVALID_DURATION =
+            "Lesson duration must be at least " + MINIMUM_DURATION + " minutes";
+
+    private final DayOfWeek dayOfOccurrence;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    /**
+     * Constructs a {@code Time}.
+     *
+     * @param dayOfOccurrence Which day the lesson occurs.
+     * @param startTime The time the lesson starts.
+     * @param endTime The time the lesson ends.
+     */
+    public Time(DayOfWeek dayOfOccurrence, LocalTime startTime, LocalTime endTime) {
+        requireAllNonNull(dayOfOccurrence, startTime, endTime);
+        checkArgument(startTime.isBefore(endTime), MESSAGE_CONSTRAINTS_IMPROPER_TIME);
+        checkArgument(isValidDuration(startTime, endTime), MESSAGE_CONSTRAINTS_INVALID_DURATION);
+
+        this.dayOfOccurrence = dayOfOccurrence;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * Returns true if the time interval between the given start time and the given end time
+     * is at least the minimum duration.
+     */
+    public boolean isValidDuration(LocalTime startTime, LocalTime endTime) {
+        long duration = Duration.between(startTime, endTime).toMinutes();
+        return duration >= MINIMUM_DURATION;
+    }
+
+    public DayOfWeek getDayOfOccurrence() {
+        return dayOfOccurrence;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    /**
+     * Returns true if both times have the same day of occurrence, start time and end time.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Time)) {
+            return false;
+        }
+
+        Time otherLesson = (Time) other;
+        return otherLesson.getDayOfOccurrence().equals(getDayOfOccurrence())
+                && otherLesson.getStartTime().equals(getStartTime())
+                && otherLesson.getEndTime().equals(getEndTime());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(dayOfOccurrence, startTime, endTime);
+    }
+
+    @Override
+    public String toString() {
+        return getDayOfOccurrence().getDisplayName(TextStyle.SHORT, Locale.ENGLISH) // output "Mon" instead of "Monday"
+                + " " + getStartTime() + " to " + getEndTime();
+    }
+
+}
