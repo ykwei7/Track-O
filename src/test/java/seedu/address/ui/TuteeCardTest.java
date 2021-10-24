c3e9c48a97ae5b1302b4214ad3072cec6085c9df
atyhamos <atyhalive@gmail.com>
2021-10-24 19:04:14 +0800

    Remove testability for ubuntu build for TuteeCardTest
    ubuntu does not support the `Platform.startup()` operation

--- src/test/java/seedu/address/ui/TuteeCardTest.java
+++ src/test/java/seedu/address/ui/TuteeCardTest.java
@@ -14,6 +14,7 @@ import seedu.address.model.lesson.Subject;
 import seedu.address.model.tag.Tag;
 import seedu.address.model.tutee.Tutee;
 
+
 public class TuteeCardTest {
 
     private static final int INVALID_INDEX = -1;
@@ -28,65 +29,67 @@ public class TuteeCardTest {
     // Solution to initialise JavaFX toolkit below adapted from https://stackoverflow.com/a/38883519
     @Test
     public void constructor_invalidTutee_throwsAssertionError() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            assertThrows(AssertionError.class, () -> new TuteeCard(null, VALID_INDEX));
         }
-        assertThrows(AssertionError.class, () -> new TuteeCard(null, VALID_INDEX));
     }
 
     @Test
     public void constructor_invalidIndex_throwsAssertionError() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            assertThrows(AssertionError.class, () -> new TuteeCard(VALID_TUTEE, INVALID_INDEX));
         }
-        assertThrows(AssertionError.class, () -> new TuteeCard(VALID_TUTEE, INVALID_INDEX));
     }
 
     @Test
     public void constructor_validInput_success() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            TuteeCard validTuteeCard = new TuteeCard(VALID_TUTEE, VALID_INDEX);
         }
-        TuteeCard validTuteeCard = new TuteeCard(VALID_TUTEE, VALID_INDEX);
     }
 
     @Test
     public void addSubjectToTagTest_nullSubject_throwsNullPointerException() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            assertThrows(NullPointerException.class, () ->
+                    new TuteeCard(VALID_TUTEE, VALID_INDEX).addSubjectToTag(null));
         }
-        assertThrows(NullPointerException.class, () -> new TuteeCard(VALID_TUTEE, VALID_INDEX).addSubjectToTag(null));
     }
 
     @Test
     public void addSubjectToTagTest_invalidSubject_throwsIllegalArgumentException() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            assertThrows(IllegalArgumentException.class, () -> new TuteeCard(VALID_TUTEE, VALID_INDEX)
+                    .addSubjectToTag(new Subject("math*")));
         }
-        assertThrows(IllegalArgumentException.class, () -> new TuteeCard(VALID_TUTEE, VALID_INDEX)
-                .addSubjectToTag(new Subject("math*")));
     }
 
     @Test
     public void addSubjectToTagTest_validSubject_success() {
-        if (!initialised) {
-            Platform.startup(() -> {});
-            initialised = true;
+        if (initToolkitSuccess()) {
+            TuteeCard validTuteeCard = new TuteeCard(VALID_TUTEE_NO_TAGS, VALID_INDEX);
+            Subject validSubject = new Subject("Math");
+            validTuteeCard.addSubjectToTag(validSubject);
+
+            Subject validSubject2 = new Subject("Science");
+            validTuteeCard.addSubjectToTag(validSubject2);
+
+            List<Tag> subjectsAsTags = validTuteeCard.getTags();
+            assertEquals(validSubject.toString(), subjectsAsTags.get(0).tagName);
+            assertEquals(validSubject2.toString(), subjectsAsTags.get(1).tagName);
         }
-        TuteeCard validTuteeCard = new TuteeCard(VALID_TUTEE_NO_TAGS, VALID_INDEX);
-        Subject validSubject = new Subject("Math");
-        validTuteeCard.addSubjectToTag(validSubject);
+    }
 
-        Subject validSubject2 = new Subject("Science");
-        validTuteeCard.addSubjectToTag(validSubject2);
-
-        List<Tag> subjectsAsTags = validTuteeCard.getTags();
-        assertEquals(validSubject.toString(), subjectsAsTags.get(0).tagName);
-        assertEquals(validSubject2.toString(), subjectsAsTags.get(1).tagName);
+    private boolean initToolkitSuccess() {
+        try {
+            Platform.startup(() -> {});
+            return true;
+        } catch (UnsupportedOperationException | IllegalStateException e) {
+            if (e instanceof UnsupportedOperationException) {
+                return false; // build-Ubuntu cannot run Platform.startup
+            } else {
+                return true;
+            }
+        }
     }
 }
