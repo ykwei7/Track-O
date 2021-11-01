c21d72fb9117eb8ae9d106d407684afac3dfd17a
KengXIII <kengi0813@gmail.com>
2021-11-01 18:19:30 +0800

    Update the phone message constraints

--- src/main/java/seedu/address/model/tutee/Phone.java
+++ src/main/java/seedu/address/model/tutee/Phone.java
@@ -11,7 +11,7 @@ public class Phone {
 
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Phone numbers should only contain numbers, and it should be at least 3 digits long";
+            "Phone numbers should only contain numbers, and it should be 8 digits long excluding spaces.";
     public static final String VALIDATION_REGEX = "\\d{8}";
     public final String value;
 
